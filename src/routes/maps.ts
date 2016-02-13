7a22789d136c04a2a0e2034c35c61e99d8b95a11
RadoslavGatev <radoslav_gatev@abv.bg>
2016-02-13 23:30:14 +0200

    Implemented the most liked places screen

--- src/routes/maps.ts
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import * as express from "express";
-
-function index(req: express.Request, res: express.Response) {
-    res.render('index', {layout: "main", body: "maps"});
-}
-
-export {index}
